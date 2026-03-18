0d17efd8c2c96e6b0e761c347d0574a24a3b796b
Prasenjit Datta <Prasenjit.Datta1@gds.ey.com>
2026-03-18 10:06:50 +0000

    rectified directory structure

--- msg-queue/msg_queue.py
+++ msg-queue/msg_queue.py
@@ -20,7 +20,9 @@
 
 import json
 import redis
- 
+from dotenv import load_dotenv
+import os
+
 # -----------------------
 # HARD‑CODED CREDENTIALS
 # -----------------------
@@ -29,22 +31,31 @@ REDIS_PORT = 6379
 REDIS_USER = "myuser"
 REDIS_PASSWORD = "mypassword"
 REDIS_DB = 0
- 
+
+# Load .env file
+load_dotenv()
+
+# Read values from environment
+redis_host = os.getenv("REDIS_HOST")
+redis_port = os.getenv("REDIS_PORT")
+redis_user = os.getenv("REDIS_USERNAME")
+redis_pass = os.getenv("REDIS_PASSWORD")
+redis_db = os.getenv("REDIS_DB")
+
 # Connect with credentials
 r = redis.Redis(
-    host='redis-16124.crce281.ap-south-1-3.ec2.cloud.redislabs.com',
-    port=16124,
+    host=redis_host,
+    port=redis_port,
     decode_responses=True,
-    username="default",
-    password="136AMdYr8UIzzA0o5n2qQZIjFgBpyGb9",
-    db=0
+    username=redis_user,
+    password=redis_pass,
+    db=redis_db
 )
- 
+
 KEY = "lis-logs"
- 
+
 # Blocking tail
 while True:
     key, raw = r.brpop(KEY, timeout=0)  # blocks forever until a new log arrives
     log = json.loads(raw)
     print(log)
-
